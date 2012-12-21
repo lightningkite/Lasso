ec6f6d85c3d036e07144cc27eef9282dffa9b02d
Christopher Blay <chris.b.blay@gmail.com>
2012-12-20 18:38:21 -0700

    Added support for more kinds of form elements and modifiers. Reworked some of the flow and improved formatting. Updated the readme

--- README.md
+++ README.md
@@ -2,16 +2,18 @@
 **Lasso** is a simple tool to allow binding *form element change events* to *model properties*.  Currently, the script requires **Backbone** and **jQuery**.
 
 ### Lasso Setup
-To use, simple include your **Lasso** reference into your scope and then call lasso on the model you wish to attach your form to.
+To use, simply include your **Lasso** reference into your scope and then call lasso on the model you wish to attach your form to.
 
     var someModel = new SomeBackboneModel();
     someModel.lasso('#some-form');
-    
-You can pass in a jQuery selector or a jQuery Object.
+
+You can pass in a jQuery selector or a jQuery object. The jQuery object is useful for situations when you want to bind elements not yet attached to the DOM.
 
     someModel.lasso($('#some-form'));
+    // also
+    someModel.lasso(this.$el.find('#some-form'));
 
-Next, make sure to name each of the form elements in your form.
+Make sure to name each of the form elements in your form.
 
     <form id="some-form">
         <input type="text" name="text-element" />
@@ -20,3 +22,15 @@ Next, make sure to name each of the form elements in your form.
 **Lasso** will then attach a change event to each of the form elements in the form so that whenever they change the model properties with the corresponding name will be updated.
 
 This gives you the ability to build your form views and your models separately, but saves you from having to define change events for every element.
+
+### Modifiers
+**Lasso** also supports modifying the value displayed in a form before saving it to the model. The is especially useful for localization such as displaying a date to the user in one format but saving it to the model in another format. Just pass a second argument to lasso specifying which names should be modified and how.
+
+    var lassoModifiers = {
+        'text-element': function(val) {
+            return val + ' this modified value will be saved to the model';
+        }
+    };
+    someModel.lasso('#some-form', lassoModifiers);
+
+**Lasso** doesn't handle modifying the value of a model attribute before being displayed to a user though - that's something your template library of choice should do.
--- src/lasso.js
+++ src/lasso.js
@@ -1,42 +1,70 @@
-(function(){
+(function() {
 
-   "use strict"; // jshint ;_;
+   'use strict'; // jshint ;_;
 
     var root = this;
 
     // Require Underscore, if we're on the server, and it's not already present.
     var _ = root._;
-    if (!_ && (typeof require !== 'undefined')) _ = require('underscore');
+    if (!_ && (typeof require !== 'undefined')) {
+        _ = require('underscore');
+    }
 
     // For Backbone's purposes, jQuery, Zepto, or Ender owns the `$` variable.
     var $ = root.jQuery || root.Zepto || root.ender;
 
     var Backbone = root.Backbone;
-    if (!_ && (typeof require !== 'undefined')) _ = require('backbone');
+    if (!_ && (typeof require !== 'undefined')) {
+        _ = require('backbone');
+    }
 
     // Add the lasso method to the prototype for Backbone Models
-    Backbone.Model.prototype.lasso = function(form){
+    Backbone.Model.prototype.lasso = function(form, modifiers) {
 
-        var $form, model = this;
+        var model = this;
+        var $form;
 
         // Check to see if a string or a jQuery object has been passed
-        if(typeof form == 'string')
+        if (typeof form === 'string') {
             $form = $(form);
-        else if(form instanceof jQuery)
+        } else if (form instanceof jQuery) {
             $form = form;
-        else
+        } else {
             return false;
+        }
 
         // Attach change event listeners to all of the form elements passed
-        $form.find(':input').each(function(i,o){
+        $form.find('input, textarea, select').each(function(i, o) {
+
             var $obj = $(o);
-            $obj.on('change', function(e){
-                if($obj.attr('name')){
-                    model.set($obj.attr('name'), $obj.val());
+
+            var checkboxCallback = function(e) {
+                var name = $obj.attr('name');
+                if (name) {
+                    model.set(name, $obj.prop('checked'), {silent: true});
                 }
-            });
+            };
+
+            var genericCallback = function(e) {
+                var name = $obj.attr('name');
+                if (name) {
+                    var val = $obj.val();
+                    if (modifiers && modifiers.hasOwnProperty(name)) {
+                        val = modifiers[name](val);
+                    }
+                    model.set(name, val, {silent: true});
+                }
+            };
+
+            if ($obj.is(':checkbox')) {
+                $obj.on('click', checkboxCallback);
+            } else if ($obj.is(':radio')) {
+                $obj.on('click', genericCallback);
+            } else {
+                $obj.on('change', genericCallback);
+            }
         });
 
-    }
+    };
 
 }).call(this);
